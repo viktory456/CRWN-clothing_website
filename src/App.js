214c1029ad74ddf6c0e9348288a1b088200cd248
Viktoria <vikvons@gmail.com>
2021-09-01 19:56:04 +0200

    added checkout page, checkout item component

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx';
-// import CheckoutPage from './pages/checkout/checkout.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
@@ -102,7 +102,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        {/* <Route exact path='/checkout' component={CheckoutPage} /> */}
+        <Route exact path='/checkout' component={CheckoutPage} />
         <Route
           exact
           path='/signin'
